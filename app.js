061c2bf2007fdafac5987530f30d0094d433d861
Demo User <demouser@MacBook-Air.local>
2015-07-22 17:49:43 -0400

    With pictures

---
+++ app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $("#hob1").click(function () {
+    $("#hob1").replaceWith( " <img src='http://www.ypc.org/news/images/20142015/golden-gate-festival_chorale.jpg'> " )
+   });
+
+   $("#hob2").click(function () {
+     $("#hob2").replaceWith( " <img src='http://hsmsekeyclub.weebly.com/uploads/2/9/3/6/2936495/2463753.png'> " )
+    });
+
+    $("#hob3").click(function () {
+      $("#hob3").replaceWith( " <img src='http://www.lyndhurstnj.org/web_content/Image/yogazumba.jpg'> " )
+     });
+});
+
+//
+// function highlightRed () {
+//     $("h5").css( "background-color", "red" );
+//   }
+//   function unhighlight(){
+//     $("h5").removeAttr("style"); //or .removeProp("background-color")
+//   }
+//   $("h5").hover(highlightRed, unhighlight);
